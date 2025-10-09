7a58cbb5cd2a3198af46bd25e65b3411587053fe
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2025-10-09 22:34:36 +0100

    wip: config update

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   
   images: {
     formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [360, 640, 768, 1024, 1280, 1536],
+    imageSizes: [64, 96, 128, 256, 384],
 
     remotePatterns: [
       {
